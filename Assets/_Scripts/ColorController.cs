4fbe93341d37878089d015a6709ed671c4d6d31f
cyuun <pero7021@colorado.edu>
2020-02-18 13:11:48 -0700

    Trees now hooked up to the color system too.

--- Assets/_Scripts/ColorController.cs
+++ Assets/_Scripts/ColorController.cs
@@ -20,6 +20,10 @@ public class ColorController : MonoBehaviour
     private List<MeshRenderer> _rock3Renderers;
     private List<MeshRenderer> _rock4Renderers;
     private List<MeshRenderer> _rock5Renderers;
+    private List<MeshRenderer> _trunkRenderers;
+    private List<MeshRenderer> _tree1Renderers;
+    private List<MeshRenderer> _tree2Renderers;
+    private List<MeshRenderer> _tree3Renderers;
 
     private int _mainColorID;
     private int _celColorID;
@@ -75,6 +79,39 @@ public class ColorController : MonoBehaviour
                     break;
             }
         }
+
+        foreach (Transform tree in _terrain.transform.Find("_TreeParent"))
+        {
+            MeshRenderer[] leavesRenderers;
+            switch (tree.name)
+            {
+                case "Tree1(Clone)":
+                    leavesRenderers = tree.Find("Capsules").GetComponentsInChildren<MeshRenderer>();
+                    foreach (var leavesRenderer in leavesRenderers)
+                    {
+                        _tree1Renderers.Add(leavesRenderer);
+                    }
+                    _trunkRenderers.Add(tree.Find("DeadTree21.fbx").GetComponentInChildren<MeshRenderer>());
+                    break;
+                
+                case "Tree2(Clone)":
+                    leavesRenderers = tree.Find("Pieces").GetComponentsInChildren<MeshRenderer>();
+                    foreach (var leavesRenderer in leavesRenderers)
+                    {
+                        _tree2Renderers.Add(leavesRenderer);
+                    }
+                    //_trunkRenderers.Add((MeshRenderer)tree.Find("P3D_DeadTree002").GetComponent<SkinnedMeshRenderer>());
+                    break;
+                
+                case "Tree3(Clone)":
+                    leavesRenderers = tree.Find("Spindles").GetComponentsInChildren<MeshRenderer>();
+                    foreach (var leavesRenderer in leavesRenderers)
+                    {
+                        _tree3Renderers.Add(leavesRenderer);
+                    }
+                    break;
+            }
+        }
     }
 
     public void ChangeColors()
@@ -111,18 +148,45 @@ public class ColorController : MonoBehaviour
             rockRenderer.material.SetColor("_Color", colorPalettes[_paletteIndex].rock5[_colorBase + _colorIndex]);
             rockRenderer.material.SetColor("_ColorDim", colorPalettes[_paletteIndex].rock5[_colorBase + (_colorIndex + 1) % 2]);
         }
+        foreach (var trunkRenderer in _trunkRenderers)
+        {
+            trunkRenderer.material.SetColor("_Color", colorPalettes[_paletteIndex].treeTrunks[_colorIndex]);
+            trunkRenderer.material.SetColor("_ColorDim", colorPalettes[_paletteIndex].treeTrunks[(_colorIndex + 1) % 2]);
+        }
+        foreach (var treeRenderer in _tree1Renderers)
+        {
+            treeRenderer.material.SetColor("_Color", colorPalettes[_paletteIndex].tree1[_colorBase + _colorIndex]);
+            treeRenderer.material.SetColor("_ColorDim", colorPalettes[_paletteIndex].tree1[_colorBase + (_colorIndex + 1) % 2]);
+        }
+        foreach (var treeRenderer in _tree2Renderers)
+        {
+            treeRenderer.material.SetColor("_Color", colorPalettes[_paletteIndex].tree2[_colorBase + _colorIndex]);
+            treeRenderer.material.SetColor("_ColorDim", colorPalettes[_paletteIndex].tree2[_colorBase + (_colorIndex + 1) % 2]);
+        }
+        foreach (var treeRenderer in _tree3Renderers)
+        {
+            treeRenderer.material.SetColor("_Color", colorPalettes[_paletteIndex].tree3[_colorBase + _colorIndex]);
+            treeRenderer.material.SetColor("_ColorDim", colorPalettes[_paletteIndex].tree3[_colorBase + (_colorIndex + 1) % 2]);
+        }
         
         _colorIndex = (_colorIndex + 1) % 2;
     }
 
     private void Start()
     {
+        _mainColorID = Shader.GetGlobalInt("_Color");
+        _celColorID = Shader.GetGlobalInt("_ColorDim");
+
         _rock1Renderers = new List<MeshRenderer>();
         _rock2Renderers = new List<MeshRenderer>();
         _rock3Renderers = new List<MeshRenderer>();
         _rock4Renderers = new List<MeshRenderer>();
         _rock5Renderers = new List<MeshRenderer>();
-        
+        _trunkRenderers = new List<MeshRenderer>();
+        _tree1Renderers = new List<MeshRenderer>();
+        _tree2Renderers = new List<MeshRenderer>();
+        _tree3Renderers = new List<MeshRenderer>();
+
         S = this;
     }
 }
