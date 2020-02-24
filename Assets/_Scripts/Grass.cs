56d5b2ffa787044c85f072f0737e2631a4912811
cyuun <pero7021@colorado.edu>
2020-02-23 22:57:50 -0700

    Started initial stages of creating grass.

---
+++ Assets/_Scripts/Grass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grass : MonoBehaviour
+{
+    [Range(0, 1)] public float grassPatchSize;
+    public TerrainScript terrainScript;
+
+    private System.Random _rng;
+    private Vector2 _offsets;
+
+    public void GenerateGrass()
+    {
+        // get perlin
+        // create mesh wherever perlin is less than grass patch size
+    }
+
+    private void Start()
+    {
+        
+    }
+}
