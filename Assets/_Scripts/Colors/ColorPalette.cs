3f2dc04506f6f38d55724e12e0f910273fa81832
cyuun <pero7021@colorado.edu>
2020-03-13 23:07:44 -0600

    Palette sizes expanded for the 3 color system.
    Former-commit-id: b0ec25fd4f89fee58e43ba1231771b52d2b366f0

--- Assets/_Scripts/Colors/ColorPalette.cs
+++ Assets/_Scripts/Colors/ColorPalette.cs
@@ -7,19 +7,19 @@ public class ColorPalette
 {
     public Color[] terrain = new Color[7]; //missing
     public Color[] grass = new Color[6];
-    public Color[] plant1 = new Color[6];
-    public Color[] plant2 = new Color[6];
-    public Color[] plant3 = new Color[6];
-    public Color[] plantBase = new Color[2];
-    public Color[] rock1 = new Color[6];
-    public Color[] rock2 = new Color[6];
-    public Color[] rock3 = new Color[6];
-    public Color[] rock4 = new Color[6]; //missing
-    public Color[] rock5 = new Color[6]; //missing
-    public Color[] wings1 = new Color[6];
-    public Color[] wings2 = new Color[6];
-    public Color[] bugBodies = new Color[6];
-    public Color[] fish1 = new Color[6];
-    public Color[] fish2 = new Color[6];
+    public Color[] plant1 = new Color[9];
+    public Color[] plant2 = new Color[9];
+    public Color[] plant3 = new Color[9];
+    public Color[] plantBase = new Color[3];
+    public Color[] rock1 = new Color[9];
+    public Color[] rock2 = new Color[9];
+    public Color[] rock3 = new Color[9];
+    public Color[] rock4 = new Color[9]; //missing
+    public Color[] rock5 = new Color[9]; //missing
+    public Color[] wings1 = new Color[9];
+    public Color[] wings2 = new Color[9];
+    public Color[] bugBodies = new Color[9];
+    public Color[] fish1 = new Color[9];
+    public Color[] fish2 = new Color[9];
     public Color[] head = new Color[3];
 }
